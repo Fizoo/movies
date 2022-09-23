4da3996fb47c219d6b5cea50d30c772ba67dbdc9
Fizoo <oleg.mykytchuk@gmail.com>
2022-09-23 12:42:58 +0300

    Auto-generated commit [ci skip]

---
+++ assets/data/dataSort.ts
@@ -0,0 +1,31 @@
+
+export const sortBy=[
+  {name:'Popularity' ,fn:'popularity.desc'},
+  {name:'dateDown' ,fn:'release_date.desc'},
+  {name:'dateUp' ,fn:'release_date.asc'},
+  {name:'MoneyInWorld' ,fn:'revenue.desc'},
+  {name:'titleDown' ,fn:'original_title.desc'},
+  {name:'titleUp' ,fn:'original_title.asc'},
+  {name:'averageVote' ,fn:'vote_average.desc'},
+  {name:'countVote' ,fn:'vote_count.desc'},
+
+]
+
+/*
+export const sortBy=[
+  'popularity.asc',
+  'popularity.desc',
+  'release_date.asc',
+  'release_date.desc',
+  'revenue.asc',
+  'revenue.desc',
+  'primary_release_date.asc',
+  'primary_release_date.desc',
+  'original_title.asc',
+  'original_title.desc',
+  'vote_average.asc',
+  'vote_average.desc',
+  'vote_count.asc',
+  'vote_count.desc'
+]
+*/
